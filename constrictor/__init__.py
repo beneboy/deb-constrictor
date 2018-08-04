a571f088fc479001fc79d8a4abe64e20467d4383
Ben Shaw <ben@beneboy.co.nz>
2018-08-04 22:03:59 +1200

    Bumped version to 0.3.1

--- constrictor/__init__.py
+++ constrictor/__init__.py
@@ -2,7 +2,7 @@ from .dpkg import DPKGBuilder
 from .control import BinaryControl
 
 __title__ = 'deb-constrictor'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __author__ = 'Ben Shaw'
 __license__ = 'BSD 3 Clause'
-__copyright__ = 'Copyright 2016 Ben Shaw'
+__copyright__ = 'Copyright 2016-2018 Ben Shaw'
